0244547c96ac259b9dc6727b417fc57b0b7b6cbc
ButterBrot777 <korendos@gmail.com>
2022-11-07 18:31:18 +0300

    feat: create new article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @artice = Article.new(article_params)
+    @artice.save
+    flash[:success] = 'Article has been created'
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end

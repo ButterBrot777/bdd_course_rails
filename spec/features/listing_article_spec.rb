7ed3bd28ae868d544772065745a68848c310f632
ButterBrot777 <korendos@gmail.com>
2022-11-09 16:03:55 +0300

    feat: add tests for listing articles

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.feature 'Listing Articles' do
   before do
     @article1 = Article.create(title: 'The first article',
@@ -19,4 +20,18 @@ RSpec.feature 'Listing Articles' do
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
   end
+
+  scenario 'A user has no articles' do
+    Article.delete_all
+    visit '/'
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+
+    expect(page).to have_content('No Articles Created')
+  end
 end
+# rubocop:enable Metrics/BlockLength
